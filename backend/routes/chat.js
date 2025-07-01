a945368f5734f585d9dca29a1eb5603dc2aa613c
adem <a.nouaouer@esi-sba.dz>
2025-07-01 13:30:07 +0100

    Clean reset with proper klch

--- backend/routes/chat.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { authenticateJWT, isChild } = require('../middleware/auth');
-const chatController = require('../controllers/chatController');
-const router = express.Router();
-
-// POST /api/chat
-router.post('/', authenticateJWT, isChild, chatController.childChat);
-
-module.exports = router;
